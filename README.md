fddccecd8977306ee846add181591f6c5f8164e8
Ian Rokas <grokas@splunk.com>
2025-03-18 05:35:13 +0000

    chore(ci): workflow updates and pre-commit changes and configurations added and applied
    [batch update]

--- README.md
+++ README.md
@@ -1,65 +1,88 @@
-[comment]: # "Auto-generated SOAR connector documentation"
 # NIST NVD
 
-Publisher: Trisha Pancho  
-Connector Version: 1.0.1  
-Product Vendor: NIST  
-Product Name: NVD  
-Product Version Supported (regex): ".\*"  
-Minimum Product Version: 5.3.2.88192  
+Publisher: Trisha Pancho \
+Connector Version: 1.0.1 \
+Product Vendor: NIST \
+Product Name: NVD \
+Minimum Product Version: 5.3.2.88192
 
 App to query NIST for CVEs
 
-### Configuration Variables
-The below configuration variables are required for this Connector to operate.  These variables are specified when configuring a NVD asset in SOAR.
+### Configuration variables
+
+This table lists the configuration variables required to operate NIST NVD. These variables are specified when configuring a NVD asset in Splunk SOAR.
 
 VARIABLE | REQUIRED | TYPE | DESCRIPTION
 -------- | -------- | ---- | -----------
-**api_version** |  required  | string | Supported API Version
+**api_version** | required | string | Supported API Version |
 
-### Supported Actions  
-[test connectivity](#action-test-connectivity) - Validate the asset configuration for connectivity using supplied configuration  
-[cve lookup](#action-cve-lookup) - Query NVD for specific CVE ID  
+### Supported Actions
+
+[test connectivity](#action-test-connectivity) - Validate the asset configuration for connectivity using supplied configuration \
+[cve lookup](#action-cve-lookup) - Query NVD for specific CVE ID
 
 ## action: 'test connectivity'
+
 Validate the asset configuration for connectivity using supplied configuration
 
-Type: **test**  
+Type: **test** \
 Read only: **True**
 
 #### Action Parameters
+
 No parameters are required for this action
 
 #### Action Output
-No Output  
+
+No Output
 
 ## action: 'cve lookup'
+
 Query NVD for specific CVE ID
 
-Type: **generic**  
+Type: **generic** \
 Read only: **False**
 
 #### Action Parameters
+
 PARAMETER | REQUIRED | DESCRIPTION | TYPE | CONTAINS
 --------- | -------- | ----------- | ---- | --------
-**cve** |  required  | CVE ID | string | 
+**cve** | required | CVE ID | string | |
 
 #### Action Output
+
 DATA PATH | TYPE | CONTAINS | EXAMPLE VALUES
 --------- | ---- | -------- | --------------
-action_result.parameter.cve | string |  |  
-action_result.data.\*.vulnerabilities.\*.cve.cisaVulnerabilityName | string |  |  
-action_result.data.\*.vulnerabilities.\*.cve.descriptions.\*.value | string |  |  
-action_result.data.\*.vulnerabilities.\*.cve.references.\*.url | string |  |  
-action_result.data.\*.vulnerabilities.\*.cve.published | string |  |  
-action_result.data.\*.vulnerabilities.\*.cve.metrics.cvssMetricV31.\*.cvssData.baseScore | string |  |  
-action_result.data.\*.vulnerabilities.\*.cve.metrics.cvssMetricV31.\*.cvssData.baseSeverity | string |  |  
-action_result.data.\*.vulnerabilities.\*.cve.metrics.cvssMetricV31.\*.cvssData.attackVector | string |  |  
-action_result.data.\*.vulnerabilities.\*.cve.metrics.cvssMetricV31.\*.impactScore | string |  |  
-action_result.data.\*.vulnerabilities.\*.cve.metrics.cvssMetricV31.\*.exploitabilityScore | string |  |  
-action_result.data.\*.vulnerabilities.\*.cve.metrics.cvssMetricV31.\*.cvssData.attackComplexity | string |  |  
-action_result.status | string |  |   success  failed 
-action_result.message | string |  |  
-summary.total_objects | numeric |  |  
-summary.total_objects_successful | numeric |  |  
-action_result.summary | string |  |  
+action_result.parameter.cve | string | | |
+action_result.data.\*.vulnerabilities.\*.cve.cisaVulnerabilityName | string | | |
+action_result.data.\*.vulnerabilities.\*.cve.descriptions.\*.value | string | | |
+action_result.data.\*.vulnerabilities.\*.cve.references.\*.url | string | | |
+action_result.data.\*.vulnerabilities.\*.cve.published | string | | |
+action_result.data.\*.vulnerabilities.\*.cve.metrics.cvssMetricV31.\*.cvssData.baseScore | string | | |
+action_result.data.\*.vulnerabilities.\*.cve.metrics.cvssMetricV31.\*.cvssData.baseSeverity | string | | |
+action_result.data.\*.vulnerabilities.\*.cve.metrics.cvssMetricV31.\*.cvssData.attackVector | string | | |
+action_result.data.\*.vulnerabilities.\*.cve.metrics.cvssMetricV31.\*.impactScore | string | | |
+action_result.data.\*.vulnerabilities.\*.cve.metrics.cvssMetricV31.\*.exploitabilityScore | string | | |
+action_result.data.\*.vulnerabilities.\*.cve.metrics.cvssMetricV31.\*.cvssData.attackComplexity | string | | |
+action_result.status | string | | success failed |
+action_result.message | string | | |
+summary.total_objects | numeric | | |
+summary.total_objects_successful | numeric | | |
+action_result.summary | string | | |
+
+______________________________________________________________________
+
+Auto-generated Splunk SOAR Connector documentation.
+
+Copyright 2025 Splunk Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
--- nistnvd_connector.py
+++ nistnvd_connector.py
@@ -1,6 +1,6 @@
 # File: nistnvd_connector.py
 
-# Copyright (c) Trisha Pancho, 2023
+# Copyright (c) Trisha Pancho, 2023-2025
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,17 +25,14 @@ from phantom.base_connector import BaseConnector
 
 
 class RetVal(tuple):
-
     def __new__(cls, val1, val2=None):
         return tuple.__new__(RetVal, (val1, val2))
 
 
 class NistNvdConnector(BaseConnector):
-
     def __init__(self):
-
         # Call the BaseConnectors init first
-        super(NistNvdConnector, self).__init__()
+        super().__init__()
 
         self._state = None
 
@@ -49,9 +46,8 @@ class NistNvdConnector(BaseConnector):
             return RetVal(phantom.APP_SUCCESS, {})
 
         return RetVal(
-            action_result.set_status(
-                phantom.APP_ERROR, f"Status Code: {response.status_code}. Empty response and no information in the header"
-            ), None
+            action_result.set_status(phantom.APP_ERROR, f"Status Code: {response.status_code}. Empty response and no information in the header"),
+            None,
         )
 
     def _process_html_response(self, response, action_result):
@@ -64,15 +60,15 @@ class NistNvdConnector(BaseConnector):
             for element in soup(["script", "style", "footer", "nav"]):
                 element.extract()
             error_text = soup.text
-            split_lines = error_text.split('\n')
+            split_lines = error_text.split("\n")
             split_lines = [x.strip() for x in split_lines if x.strip()]
-            error_text = '\n'.join(split_lines)
+            error_text = "\n".join(split_lines)
         except:
             error_text = "Cannot parse error details"
 
-        message = "Status Code: {0}. Data from server:\n{1}\n".format(status_code, error_text)
+        message = f"Status Code: {status_code}. Data from server:\n{error_text}\n"
 
-        message = message.replace(u'{', '{{').replace(u'}', '}}')
+        message = message.replace("{", "{{").replace("}", "}}")
         return RetVal(action_result.set_status(phantom.APP_ERROR, message), None)
 
     def _process_json_response(self, r, action_result):
@@ -80,42 +76,35 @@ class NistNvdConnector(BaseConnector):
         try:
             resp_json = r.json()
         except Exception as e:
-            return RetVal(
-                action_result.set_status(
-                    phantom.APP_ERROR, "Unable to parse JSON response. Error: {0}".format(str(e))
-                ), None
-            )
+            return RetVal(action_result.set_status(phantom.APP_ERROR, f"Unable to parse JSON response. Error: {e!s}"), None)
 
         # Please specify the status codes here
         if 200 <= r.status_code < 399:
             return RetVal(phantom.APP_SUCCESS, resp_json)
 
         # You should process the error returned in the json
-        message = "Error from server. Status Code: {0} Data from server: {1}".format(
-            r.status_code,
-            r.text.replace(u'{', '{{').replace(u'}', '}}')
-        )
+        message = "Error from server. Status Code: {} Data from server: {}".format(r.status_code, r.text.replace("{", "{{").replace("}", "}}"))
 
         return RetVal(action_result.set_status(phantom.APP_ERROR, message), None)
 
     def _process_response(self, r, action_result):
         # store the r_text in debug data, it will get dumped in the logs if the action fails
-        if hasattr(action_result, 'add_debug_data'):
-            action_result.add_debug_data({'r_status_code': r.status_code})
-            action_result.add_debug_data({'r_text': r.text})
-            action_result.add_debug_data({'r_headers': r.headers})
+        if hasattr(action_result, "add_debug_data"):
+            action_result.add_debug_data({"r_status_code": r.status_code})
+            action_result.add_debug_data({"r_text": r.text})
+            action_result.add_debug_data({"r_headers": r.headers})
 
         # Process each 'Content-Type' of response separately
 
         # Process a json response
-        if 'json' in r.headers.get('Content-Type', ''):
+        if "json" in r.headers.get("Content-Type", ""):
             return self._process_json_response(r, action_result)
 
         # Process an HTML response, Do this no matter what the api talks.
         # There is a high chance of a PROXY in between phantom and the rest of
         # world, in case of errors, PROXY's return HTML, this function parses
         # the error and adds it to the action_result.
-        if 'html' in r.headers.get('Content-Type', ''):
+        if "html" in r.headers.get("Content-Type", ""):
             return self._process_html_response(r, action_result)
 
         # it's not content-type that is to be parsed, handle an empty response
@@ -123,9 +112,8 @@ class NistNvdConnector(BaseConnector):
             return self._process_empty_response(r, action_result)
 
         # everything else is actually an error at this point
-        message = "Can't process response from server. Status Code: {0} Data from server: {1}".format(
-            r.status_code,
-            r.text.replace('{', '{{').replace('}', '}}')
+        message = "Can't process response from server. Status Code: {} Data from server: {}".format(
+            r.status_code, r.text.replace("{", "{{").replace("}", "}}")
         )
 
         return RetVal(action_result.set_status(phantom.APP_ERROR, message), None)
@@ -140,10 +128,7 @@ class NistNvdConnector(BaseConnector):
         try:
             request_func = getattr(requests, method)
         except AttributeError:
-            return RetVal(
-                action_result.set_status(phantom.APP_ERROR, "Invalid method: {0}".format(method)),
-                resp_json
-            )
+            return RetVal(action_result.set_status(phantom.APP_ERROR, f"Invalid method: {method}"), resp_json)
 
         # Create a URL to connect to
         self.debug_print(self._base_url)
@@ -153,15 +138,11 @@ class NistNvdConnector(BaseConnector):
             r = request_func(
                 url,
                 # auth=(username, password),  # basic authentication
-                verify=config.get('verify_server_cert', False),
-                **kwargs
+                verify=config.get("verify_server_cert", False),
+                **kwargs,
             )
         except Exception as e:
-            return RetVal(
-                action_result.set_status(
-                    phantom.APP_ERROR, "Error Connecting to server. Details: {0}".format(str(e))
-                ), resp_json
-            )
+            return RetVal(action_result.set_status(phantom.APP_ERROR, f"Error Connecting to server. Details: {e!s}"), resp_json)
 
         return self._process_response(r, action_result)
 
@@ -177,7 +158,7 @@ class NistNvdConnector(BaseConnector):
         self.save_progress("Connecting to endpoint")
         # make rest call
         ret_val, response = self._make_rest_call(
-            '/rest/json/cves/2.0?cveId=CVE-2019-1010218', action_result, method='get', params=None, headers=None
+            "/rest/json/cves/2.0?cveId=CVE-2019-1010218", action_result, method="get", params=None, headers=None
         )
 
         if phantom.is_fail(ret_val):
@@ -193,7 +174,7 @@ class NistNvdConnector(BaseConnector):
     def _handle_cve_lookup(self, param):
         # Implement the handler here
         # use self.save_progress(...) to send progress messages back to the platform
-        self.save_progress("In action handler for: {0}".format(self.get_action_identifier()))
+        self.save_progress(f"In action handler for: {self.get_action_identifier()}")
 
         # Add an action result object to self (BaseConnector) to represent the action for this param
         action_result = self.add_action_result(ActionResult(dict(param)))
@@ -201,7 +182,7 @@ class NistNvdConnector(BaseConnector):
         # Access action parameters passed in the 'param' dictionary
 
         # Required values can be accessed directly
-        cve = param['cve']
+        cve = param["cve"]
 
         # Optional values should use the .get() function
         # optional_parameter = param.get('optional_parameter', 'default_value')
@@ -209,9 +190,7 @@ class NistNvdConnector(BaseConnector):
         endpoint = "/rest/json/cves/" + self._api_version + "?cveId=" + cve
 
         # make rest call
-        ret_val, response = self._make_rest_call(
-            endpoint, action_result, method='get', params=None, headers=None
-        )
+        ret_val, response = self._make_rest_call(endpoint, action_result, method="get", params=None, headers=None)
 
         if phantom.is_fail(ret_val):
             # the call to the 3rd party device or service failed, action result should contain all the error details
@@ -244,10 +223,10 @@ class NistNvdConnector(BaseConnector):
 
         self.debug_print("action_id", self.get_action_identifier())
 
-        if action_id == 'cve_lookup':
+        if action_id == "cve_lookup":
             ret_val = self._handle_cve_lookup(param)
 
-        if action_id == 'test_connectivity':
+        if action_id == "test_connectivity":
             ret_val = self._handle_test_connectivity(param)
 
         return ret_val
@@ -285,10 +264,10 @@ def main():
 
     argparser = argparse.ArgumentParser()
 
-    argparser.add_argument('input_test_json', help='Input Test JSON file')
-    argparser.add_argument('-u', '--username', help='username', required=False)
-    argparser.add_argument('-p', '--password', help='password', required=False)
-    argparser.add_argument('-v', '--verify', action='store_true', help='verify', required=False, default=False)
+    argparser.add_argument("input_test_json", help="Input Test JSON file")
+    argparser.add_argument("-u", "--username", help="username", required=False)
+    argparser.add_argument("-p", "--password", help="password", required=False)
+    argparser.add_argument("-v", "--verify", action="store_true", help="verify", required=False, default=False)
 
     args = argparser.parse_args()
     session_id = None
@@ -298,31 +277,31 @@ def main():
     verify = args.verify
 
     if username is not None and password is None:
-
         # User specified a username but not a password, so ask
         import getpass
+
         password = getpass.getpass("Password: ")
 
     if username and password:
         try:
-            login_url = NistNvdConnector._get_phantom_base_url() + '/login'
+            login_url = NistNvdConnector._get_phantom_base_url() + "/login"
 
             print("Accessing the Login page")
             r = requests.get(login_url, verify=verify)
-            csrftoken = r.cookies['csrftoken']
+            csrftoken = r.cookies["csrftoken"]
 
             data = dict()
-            data['username'] = username
-            data['password'] = password
-            data['csrfmiddlewaretoken'] = csrftoken
+            data["username"] = username
+            data["password"] = password
+            data["csrfmiddlewaretoken"] = csrftoken
 
             headers = dict()
-            headers['Cookie'] = 'csrftoken=' + csrftoken
-            headers['Referer'] = login_url
+            headers["Cookie"] = "csrftoken=" + csrftoken
+            headers["Referer"] = login_url
 
             print("Logging into Platform to get the session id")
             r2 = requests.post(login_url, verify=verify, data=data, headers=headers)
-            session_id = r2.cookies['sessionid']
+            session_id = r2.cookies["sessionid"]
         except Exception as e:
             print("Unable to get session id from the platform. Error: " + str(e))
             sys.exit(1)
@@ -336,8 +315,8 @@ def main():
         connector.print_progress_message = True
 
         if session_id is not None:
-            in_json['user_session_token'] = session_id
-            connector._set_csrf_info(csrftoken, headers['Referer'])
+            in_json["user_session_token"] = session_id
+            connector._set_csrf_info(csrftoken, headers["Referer"])
 
         ret_val = connector._handle_action(json.dumps(in_json), None)
         print(json.dumps(json.loads(ret_val), indent=4))
@@ -345,5 +324,5 @@ def main():
     sys.exit(0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
